48b25e756c06186c7f62ef95a056f9f91c372199
Denis <oktilon@gmail.com>
2025-04-24 18:34:46 +0200

    Use gpio v2 monitoring

--- app.c
+++ app.c
@@ -5,18 +5,50 @@
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <linux/gpio.h>
+#include <linux/types.h>
 
 #define GPIO3                       "/dev/gpiochip3"
 #define GPIO_BUTTON                 23 // RK_PC7
+#define BUTTON_DEBOUNCE_US          50
 
 static struct gpiohandle_request    btn = {0};
+static struct gpio_v2_line_config   config;
+static struct gpio_v2_line_request  req;
+static struct gpio_v2_line_values   values;
+
+
+/* helper functions for gpio_v2_line_values bits */
+static inline void pin_set_bit(__u64 *b, int n) {
+	*b |= _BITULL(n);
+}
+
+static inline void pin_change_bit(__u64 *b, int n) {
+	*b ^= _BITULL(n);
+}
+
+static inline void pin_clear_bit(__u64 *b, int n) {
+	*b &= ~_BITULL(n);
+}
+
+static inline int pin_test_bit(__u64 b, int n) {
+	return !!(b & _BITULL(n));
+}
+
+static inline void gpiotools_assign_bit(__u64 *b, int n, bool value) {
+	if (value)
+		gpiotools_set_bit(b, n);
+	else
+		gpiotools_clear_bit(b, n);
+}
+
 
 int main(int argc, char **argv) {
-    int fd, r;
+    int fd, r, ret = 0;
 
     printf("Opening %s...\n", GPIO3);
     fd = open(GPIO3, O_RDWR);
@@ -26,22 +58,91 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    /* Setup Button GPIO to input */
-    btn.flags = GPIOHANDLE_REQUEST_INPUT;
-    strcpy(btn.consumer_label, "Button");
-    memset(btn.default_values, 0, sizeof(btn.default_values));
-    btn.lines = 1;
-    btn.lineoffsets[0] = GPIO_BUTTON;
+    memset(&config, 0, sizeof(config));
+    config.flags = GPIO_V2_LINE_FLAG_INPUT | GPIO_V2_LINE_FLAG_EDGE_FALLING | GPIO_V2_LINE_EVENT_RISING_EDGE;
+    config.attrs[0].attr.id = GPIO_V2_LINE_ATTR_ID_DEBOUNCE;
+    config.attrs[0].attr.debounce_period_us = BUTTON_DEBOUNCE_US;
+    config.num_attrs = 1;
 
-    if ((r = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &btn)) < 0) {
-        printf("Init GPIO%d (Button) error(%d): %m", GPIO_BUTTON, errno);
-    } else {
-        printf("Init GPIO%d (Button) ok(%d)", GPIO_BUTTON, r);
+    memset(&req, 0, sizeof(req));
+    req.offsets[0] = GPIO_BUTTON;
+    req.num_lines = 1;
+    req.config = config;
+    strcpy(req.consumer, "Button");
+
+    r = ioctl(fd, GPIO_V2_GET_LINE_IOCTL, &req);
+
+    if (r < 0) {
+        printf("Init GPIO%d (Button) error(%d): %m\n", GPIO_BUTTON, errno);
+        ret = r;
+        goto exit_device_close;
+    }
+    printf("Init GPIO%d (Button) ok(%d)\n", GPIO_BUTTON, r);
+
+
+    /* Read initial states */
+    values.mask = 0;
+    values.bits = 0;
+    gpiotools_set_bit(&values.mask, 0);
+
+    r = ioctl(req.fd, GPIO_V2_LINE_GET_VALUES_IOCTL, values);
+    if (r < 0) {
+        printf("Get GPIO%d (Button) value error(%d): %m\n", GPIO_BUTTON, errno);
+        ret = r;
+        goto exit_line_close;
     }
 
-    printf("Closing...\n");
+    printf("Initial line value: %d\n",
+        gpiotools_test_bit(values.bits, 0));
+
+    while (1) {
+        struct gpio_v2_line_event event;
+
+        r = read(req.fd, &event, sizeof(event));
+        if (r < 0) {
+            if (errno == -EAGAIN) {
+                printf("nothing available\n");
+                continue;
+            } else {
+                printf("Failed to read event (%d): %m\n", errno);
+                ret = -1;
+                break;
+            }
+        }
+
+        if (r != sizeof(event)) {
+            printf("Reading event failed\n");
+            ret = -EIO;
+            break;
+        }
+
+        printf("GPIO EVENT at %lu on line %d (%d|%d) "
+            , event.timestamp_ns
+            , event.offset
+            , event.line_seqno
+            , event.seqno);
+
+        switch (event.id) {
+        case GPIO_V2_LINE_EVENT_RISING_EDGE:
+            printf("rising edge");
+            break;
+        case GPIO_V2_LINE_EVENT_FALLING_EDGE:
+            printf("falling edge");
+            break;
+        default:
+            printf("unknown event");
+        }
+        printf("\n");
+
+    }
+
+exit_line_close:
+    printf("Closing line...\n");
+    close(req.fd);
+exit_device_close:
+    printf("Closing chip...\n");
     close(fd);
 
     printf("Exit\n");
-    return 0;
+    return ret;
 }
